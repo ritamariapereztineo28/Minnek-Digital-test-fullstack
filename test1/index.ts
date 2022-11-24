087269f60e0f98c8137232a5d6aa2ae5cbad7a83
ritamariapereztineo28 <ritamariapereztineo@gmail.com>
2022-11-24 14:54:30 -0400

    fixes

--- test1/index.ts
+++ test1/index.ts
@@ -16,24 +16,29 @@ const arr = [
   "%",
   8,
 ];
-
 const spliceFunction = (result, addPosition, value) => {
   result.splice(addPosition, 0, value);
 };
-const arrReverse = (arr, inverseArray = [], specialCharactersArr = []) => {
-  arr.map((i) => {
+const arrReverse = (
+  arr,
+  inverseArray = [],
+  positionSpecialCharactersArr = [],
+  specialCharactersArr = []
+) => {
+  arr.map((i, key) => {
     const specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(i);
     if (specialCharacters === false) {
       inverseArray.push(i);
     } else {
+      positionSpecialCharactersArr.push(key);
       specialCharactersArr.push(i);
     }
   });
   const result = inverseArray.reverse();
-  spliceFunction(result, 2, specialCharactersArr[0]);
-  spliceFunction(result, 6, specialCharactersArr[1]);
-  spliceFunction(result, 14, specialCharactersArr[2]);
-
+  spliceFunction(result, positionSpecialCharactersArr[0], specialCharactersArr[0]);
+  spliceFunction(result, positionSpecialCharactersArr[1], specialCharactersArr[1]);
+  spliceFunction(result, positionSpecialCharactersArr[2], specialCharactersArr[2]);
   console.log("El resultado esperado es : ", result);
+
 };
-arrReverse(arr, [], []);
+arrReverse(arr, [], [], []);
