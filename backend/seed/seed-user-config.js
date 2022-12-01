8207e64df52929657a2b8723407d104cad7d27a5
ritamariapereztineo28 <ritamariapereztineo@gmail.com>
2022-11-30 23:30:55 -0400

    add: seed script and other fix

--- backend/seed/seed-user-config.js
+++ backend/seed/seed-user-config.js
@@ -3,12 +3,9 @@ const { DATABASE } = require("../config-env.ts");
 
 try {
   execSync(
-    `mongoimport --db ${DATABASE} --collection users --drop --file "./seed/seed-user.json" --jsonArray`
+    `mongoimport --db ${DATABASE} --collection users --drop --file "./seed/seed-user-data.json" --jsonArray`
   );
   console.log("SEED RUNNING 🏃🏻🏃🏻🏃🏻");
 } catch (err) {
-  console.log(
-    `Failed to import file into database ${DATABASE}`,
-    err
-  );
+  console.log(`Failed to import file into database ${DATABASE}`, err);
 }
