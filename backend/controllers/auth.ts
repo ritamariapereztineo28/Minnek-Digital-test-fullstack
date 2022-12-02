82222c10ee1f5a37b9244d37e8e648b02fa6d7bc
ritamariapereztineo28 <ritamariapereztineo@gmail.com>
2022-12-02 19:08:20 -0400

    fixes

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -4,6 +4,8 @@ const { JWT_SECRET } = require("../config-env");
 export const auth = async (req: any, res: any, next: any) => {
   try {
     const token = req.headers.authorization;
+    // console.log('===========',req.headers.authorization)
+    // console.log("--------->",req.signedCookies.auth_token)
     const authEntity = jwt.verify(token, JWT_SECRET);
     Object.assign(req, { authEntity });
     next();
