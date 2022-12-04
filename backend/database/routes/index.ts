c6cb5581ef34aad55e3d250304d1960423b6f0e4
ritamariapereztineo28 <ritamariapereztineo@gmail.com>
2022-12-04 19:56:55 -0400

    creating product remove endpoint

--- backend/database/routes/index.ts
+++ backend/database/routes/index.ts
@@ -3,6 +3,7 @@ import { getProducts } from "../../controllers/getProducts";
 import { login } from "../../controllers/login";
 import { authenticator } from "../authenticator";
 import { auth } from "../../controllers/auth";
+import { removeAllProducts } from "../../controllers/removeAllProducts";
 
 const expRouter = require("express");
 const router = expRouter.Router();
@@ -10,6 +11,7 @@ const router = expRouter.Router();
 router.post("/auth", authenticator, auth);
 router.post("/product", authenticator, addProduct);
 router.get("/products", authenticator, getProducts);
+router.delete("/products", removeAllProducts);
 router.post("/login", login);
 
 module.exports = router;
